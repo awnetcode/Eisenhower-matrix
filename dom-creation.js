5c4c8cf4c9178e4aea48666d6e474868600e5978
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-11-08 11:42:53 +0100

    initial

---
+++ dom-creation.js
@@ -0,0 +1,25 @@
+const mainContent = document.querySelector("#root");
+
+const createQuartElement = (quartName, quartText) =>{
+    const quarter = document.createElement("div");
+    quarter.innerText = quartText
+    quarter.classList.add(quartName);
+    return quarter;
+}
+
+const createMatrix = () =>{
+
+    const matrixElement = document.createElement("div");
+    mainContent.appendChild(matrixElement).classList.add("matrix");
+
+    const matrix = document.querySelector(".matrix");
+    matrix.appendChild(createQuartElement("first-quart", "Pilne i ważne"));
+    matrix.appendChild(createQuartElement("second-quart", "Pilne i nieważne"));
+    matrix.appendChild(createQuartElement("third-quart", "Niepilne i ważne"));
+    matrix.appendChild(createQuartElement("fourth-quart", "Niepilne i nieważne"));
+}
+
+createMatrix();
+
+//Zrobić tę wspaniałą stronę, która pomoże w końcu skupić się na tym co ważne, i wszystkie prośby innych ludzi wyjebie do kosza, gdzie ich miejsce.
+//A jeśli overflow na tym polu się wydarzy, natenczas się scroll po prawej pojawi.
